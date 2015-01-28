26d607b779eaf7f54ebb4dcfa7eecce5fc35ce67
Dannymoses <dannymoses555@gmail.com>
2015-01-27 21:19:03 -0500

    Added 2 new features
    You can now pass commands as command line arguments to be run on
    startup of the program. There is now the option to type "close"
    to exit the program.

--- main.c
+++ main.c
@@ -5,15 +5,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
+  printf("Welcome to Moses Shell, the most limited experience of your life!\n");
+  printf("Version: 0.1.updatesain'tgonnahappen\n");
+  for(int i = 1; i <= argc; i++)
+    {
+	printf("executing command: %s \n", argv[i]);
+	system(argv[i]);
+    }
 	while(1)
 	{
 		char cmd[120];
 		printf("$: ");
 		fgets(cmd, 120, stdin);
-		system(cmd);
+		if(strncmp(cmd, "close", 5) == 0)
+		{
+			break;
+		}
+		else if (strncmp(cmd, "close", 5) != 0)
+		{
+			system(cmd);
+		}
 	}
 	return 0;
 }
