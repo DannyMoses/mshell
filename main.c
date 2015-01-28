5ccc64b8b1c8d0fdf41f4fb8e45bfbec79b14f12
Dannymoses <dannymoses555@gmail.com>
2015-01-28 00:05:42 -0500

    Fixed segfaults
    There used to be a bug where the program would segfault if not
    given any command line arguments.

--- main.c
+++ main.c
@@ -9,32 +9,33 @@
 
 int main(int argc, char **argv)
 {
-  printf("Welcome to Moses Shell, the most limited experience of your life!\n");
-  printf("Version: 0.1.updatesain'tgonnahappen\n");
-  if(strcmp(argv[1], "-help") == 0)
-  {
-	printf("USAGE: mshell [commands]\n");
-	printf("simple type 'mshell' then whatever commands you want to be run at the start of the program\n");
-	return 0;
-  }
-  for(int i = 1; i <= argc; i++)
-    {
-	printf("executing command: %s \n", argv[i]);
-	system(argv[i]);
-    }
-	while(1)
-	{
-		char cmd[120];
-		printf("$: ");
-		fgets(cmd, 120, stdin);
-		if(strncmp(cmd, "close", 5) == 0)
+	printf("Welcome to Moses Shell, the most limited experience of your life!\n");
+  	printf("Version: 0.1.updatesain'tgonnahappen\n");
+	printf("Type 'help' for help.\n");
+ 	for(int i = 1; i <= argc; i++)
+ 	{
+		printf("executing command: %s \n", argv[i]);
+		system(argv[i]);
+   	}
+		while(1)
 		{
-			break;
+			char cmd[120];
+			printf("$: ");
+			fgets(cmd, 120, stdin);
+			if(strncmp(cmd, "close", 5) == 0)
+			{
+				return 0;
+			}
+			else if(strncmp(cmd, "help", 4) ==0)
+			{
+				printf("simply type 'mshell' and then begin using your favorite command line things!\n");
+				return 0;
+			}
+			else if (strncmp(cmd, "close", 5) != 0)
+			{
+				system(cmd);
+			}
 		}
-		else if (strncmp(cmd, "close", 5) != 0)
-		{
-			system(cmd);
-		}
-	}
-	return 0;
-}
+	
+return 0;
+}	
